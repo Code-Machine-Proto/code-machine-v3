dac10366010452e73d63c788c05711d59af77314
abdulchaarani <abdul@chaarani.ca>
2026-09-13 16:59:59 -0400

    feat: rename accumulator v2 to accumulator ma

--- simulator/src/engine/mod.rs
+++ simulator/src/engine/mod.rs
@@ -1,12 +1,13 @@
 pub mod accumulator;
+pub mod accumulator_ma;
 pub mod polyrisc;
 
 use crate::types::{ProcessorId, SimulationTrace};
 
 pub fn simulate(program: &[u32], processor_id: ProcessorId, data_memory: Option<&[i32]>) -> SimulationTrace {
     match processor_id {
-        ProcessorId::Accumulator => accumulator::simulate_v1(program, data_memory),
-        ProcessorId::AccumulatorMa => accumulator::simulate_v2(program, data_memory),
+        ProcessorId::Accumulator => accumulator::simulate(program, data_memory),
+        ProcessorId::AccumulatorMa => accumulator_ma::simulate(program, data_memory),
         ProcessorId::PolyRisc => polyrisc::simulate(program, data_memory),
     }
 }
